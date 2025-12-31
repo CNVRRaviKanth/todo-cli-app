0acca850d76d8d5e348f8b6dfbb9f493c1e5683a
Ravikanth <ravikanthapple489@gmail.com>
2025-12-31 14:29:00 +0530

    Delete task feature

--- todo.py
+++ todo.py
@@ -6,6 +6,14 @@ def show_tasks():
     else:
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
+def delete_task(index):
+    try:
+        tasks.pop(index - 1)
+        print("Task deleted")
+    except IndexError:
+        print("Invalid task number")
+
+
 
 if __name__ == "__main__":
     show_tasks()
